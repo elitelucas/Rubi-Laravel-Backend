fa7915dd2423c820cdd2604978bd3c4b9da1c3ca
elitelucas <onecodestar@outlook.com>
2023-07-04 16:01:06 +0200

    feat:completed admin

---
+++ app/Http/Controllers/ManageApifyController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class ManageApifyController extends Controller
+{
+    //
+    public function settingApify(Request $request)
+    {
+        try {
+            $apiUrl = 'https://api.apify.com/v2/acts/apify~web-scraper/runs';
+            $apiToken = env('APIFY_API_TOKEN');
+
+            $inputData = file_get_contents('input.json');
+
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $apiToken,
+            ])->post($apiUrl, json_decode($inputData, true));
+
+            if ($response->failed()) {
+                echo 'Error: ' . $response->body();
+            } else {
+                echo 'Scrape job started successfully!';
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            echo $e->getMessage();
+        }
+    }
+
+    public function getAllApifyStoreApps()
+    {
+        try {
+
+            $token = env('APIFY_API_TOKEN');
+
+            // Make a GET request to the Apify API with the authentication token
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $token,
+            ])->get('https://api.apify.com/v2/acts?limit=10000');
+
+            // Get the JSON response body
+            if ($response->successful()) {
+
+                $data = $response->json()['data'];
+
+                echo dd($data);
+
+                // Access the Apify Store apps
+                $apps = $data['items'];
+
+                // Loop through the apps and do something with each one
+                foreach ($apps as $app) {
+                    // Access app details
+                    $name = $app['name'];
+                    // $description = $app['description'];
+
+                    // Do something with the app details
+                    echo "Name: $name\n";
+                    // echo "Description: $description\n";
+                    echo "\n";
+                }
+            } else {
+                echo 'Error occurred: ' . $response->body();
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            echo $e->getMessage();
+        }
+    }
+}
---
+++ app/Http/Controllers/OpenAIController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use OpenAI;
+class OpenAIController extends Controller
+{
+    private $models = [
+        "babbage" => "text-babbage-001",
+        "curies" => "text-curie-001",
+        "ada" => "text-ada-001",
+        "davinci" => "davinci:ft-bitjar-labs-prod:rubi-2023-06-19-11-24-42",
+        "code_davinci" => "code-davinci-002"
+    ];
+
+    public function open_ai(Request $request)
+    {
+        $client = OpenAI::client(env('OPEN_AI_TOKEN'));
+
+        $prompt = " Rubi, can you recommend a good podcast for comedy lovers?";
+
+        $maxTokens = 250;
+
+        $result = $client->completions()->create([
+            'model' => $this->models['davinci'],
+            'prompt' => $prompt,
+            "temperature" => 0.1,
+            "max_tokens" => $maxTokens,
+            "top_p" => 1,
+            "frequency_penalty" => 0,
+            "presence_penalty" => 0,
+            "stop" => ["END"]
+        ]);
+
+        echo $result['choices'][0]['text'];
+    }
+
+}
