648649ab01af16d9fc018452261a9c21bec2db17
elitelucas <onecodestar@outlook.com>
2023-07-14 20:28:34 +0800

    Update init-rubi.sh
    Update init-rubi.sh
    
    Update init-rubi.sh
    
    Update init-rubi.sh

--- init-rubi.sh
+++ init-rubi.sh
@@ -2,12 +2,26 @@
 
 set -e
 
-composer dump-autoload
 
-/usr/local/bin/php /app/artisan optimize
 
-/usr/local/bin/php /app/artisan config:cache
-/usr/local/bin/php /app/artisan view:cache
-/usr/local/bin/php /app/artisan view:clear
 
-/usr/local/bin/php /app/artisan migrate --force
+if [ ${RUN_PRESTART:-False} ne 'False' ];
+then
+
+    /usr/local/bin/php /app/artisan migrate --force
+
+    composer dump-autoload
+
+    /usr/local/bin/php /app/artisan optimize
+
+    /usr/local/bin/php /app/artisan config:cache
+    /usr/local/bin/php /app/artisan view:cache
+    /usr/local/bin/php /app/artisan view:clear
+
+fi
+
+
+if [ ${MIGRATE_SEED:-False} ne 'False'];
+then
+    /usr/local/bin/php /app/artisan migrate:fresh --seed --force
+fi
