9b41e4e81028ad96b039ce79e369306c2dbf8dec
elitelucas <onecodestar@outlook.com>
2023-07-22 13:50:48 -0300

    feat: add vat column on users table

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,46 +16,68 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $robert = User::factory()->state([
+        $robert = User::create([
+            'uuid' => Str::uuid(),
             'firstname' => 'Robert',
             'lastname' => 'Sacco',
             'mobile' => '9186050328',
             'email' => 'robert.sacco@bitjarlabs.com',
             'username' => 'bitjarlabs',
-            'role' => 'superadmin',
+            'password' => Hash::make('123456'),
+            'role' => 'super-admin',
             'status' => 'active',
             '2fa_verified' => true,
             'date_of_birth' => '1970-10-02',
             'ip_address' => '72.100.90.263',
-        ])->create();
+        ]);
         $robert->assignRole(RoleEnum::SUPER_ADMIN->value);
 
-        $corey = User::factory()->state([
+        $flex = User::create([
+            'uuid' => Str::uuid(),
+            'firstname' => 'Flex',
+            'lastname' => 'Sacco',
+            'email' => 'admin@myflex.ai',
+            'username' => 'flex',
+            'password' => Hash::make('123456'),
+            'role' => 'client-admin',
+            'mobile' => '9186050328',
+            'status' => 'active',
+            '2fa_verified' => true,
+            'date_of_birth' => '1970-10-02',
+            'ip_address' => '72.100.90.263',
+        ]);
+        $flex->assignRole(RoleEnum::CLIENT_ADMIN->value);
+
+        $corey = User::create([
+            'uuid' => Str::uuid(),
             'firstname' => 'Corey',
             'lastname' => 'Davidson',
             'mobile' => '5122896014',
             'email' => 'corey.davidson@bitjarlabs.com',
             'username' => 'corey',
-            'role' => 'customer',
+            'password' => Hash::make('123456'),
+            'role' => 'client-customer',
             'status' => 'active',
             '2fa_verified' => false,
             'date_of_birth' => '1970-03-05',
             'ip_address' => '72.102.23.234',
-        ])->create();
+        ]);
         $corey->assignRole(RoleEnum::CUSTOMER->value);
 
-        $jay = User::factory()->state([
+        $jay = User::create([
+            'uuid' => Str::uuid(),
             'firstname' => 'Jay',
             'lastname' => 'Dixon',
             'mobile' => '9188985542',
             'email' => 'jay.dixon@bitjarlabs.com',
             'username' => 'jay',
-            'role' => 'customer',
+            'password' => Hash::make('123456'),
+            'role' => 'client-customer',
             'status' => 'active',
             '2fa_verified' => false,
             'date_of_birth' => '1989-08-23',
             'ip_address' => '74.292.213.12',
-        ])->create();
+        ]);
         $jay->assignRole(RoleEnum::CUSTOMER->value);
     }
 }
