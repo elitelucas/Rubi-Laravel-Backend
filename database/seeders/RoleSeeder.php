9be8fb73693eb2aa4165cd3acaf25590c33b149d
elitelucas <onecodestar@outlook.com>
2023-07-26 14:04:42 -0300

    fix: Role and Permission seeders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
@@ -13,10 +14,13 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::truncate();
-        Role::create(['name' => 'super-admin']);
-        Role::create(['name' => 'client-admin']);
-        Role::create(['name' => 'client-customer']);
-        Role::create(['name' => 'collaborator']);
+        /** @var Role $superAdminRole */
+        $superAdminRole = Role::firstOrcreate(['name' => 'super-admin']);
+        Role::firstOrcreate(['name' => 'client-admin']);
+        Role::firstOrcreate(['name' => 'client-customer']);
+        Role::firstOrcreate(['name' => 'collaborator']);
+
+        // Assign all permissions to Super Admin
+        $superAdminRole->givePermissionTo(Permission::all());
     }
 }
