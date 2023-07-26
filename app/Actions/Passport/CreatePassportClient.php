24143494c0392753b5708bc1e8dc4e54d5d6f27f
elitelucas <onecodestar@outlook.com>
2023-07-25 23:25:43 -0300

    fix: wrong syntax in migartion

---
+++ app/Actions/Passport/CreatePassportClient.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Actions\Passport;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Laravel\Passport\ClientRepository;
+
+
+class CreatePassportClient
+{
+    private $clientRepository;
+
+    // Injetar o ClientRepository via construtor
+    public function __construct(ClientRepository $clientRepository)
+    {
+        $this->clientRepository = $clientRepository;
+    }
+
+    public function handle($user, $client_url)
+    {
+        // Cria um novo cliente no passport usando os dados do usuário recem criado
+
+        $client = $this->clientRepository->create($user->id, $user->client->business_name, $client_url);
+        dd($client);
+//        return User::create($userData);
+    }
+}
--- database/migrations/2023_07_23_140402_create_voices_table.php
+++ database/migrations/2023_07_23_140402_create_voices_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('voices', function (Blueprint $table) {
-            $$table->id();
+            $table->id();
             $table->foreignId('personas_id')->constrained('personas');
             $table->string('tone_1');
             $table->string('tone_2');
