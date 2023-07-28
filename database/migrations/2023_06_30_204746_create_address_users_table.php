0195a02a2d4c87f3335c12424a5aa7986742bb01
elitelucas <onecodestar@outlook.com>
2023-07-28 15:13:14 -0300

    fix: more migration rollbacks

--- database/migrations/2023_06_30_204746_create_address_users_table.php
+++ database/migrations/2023_06_30_204746_create_address_users_table.php
@@ -35,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('addresses');
+        Schema::dropIfExists('address_users');
     }
 };
