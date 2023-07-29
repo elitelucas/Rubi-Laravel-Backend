c2314e5df58f9986fe828a641fb12d9d223e6908
elitelucas <onecodestar@outlook.com>
2023-07-29 19:17:19 -0300

    feat: add new laravel commando to make new actions

---
+++ app/Console/Commands/MakeActionCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Concerns\CreatesMatchingTest;
+use Illuminate\Console\GeneratorCommand;
+
+class MakeActionCommand extends GeneratorCommand
+{
+    use CreatesMatchingTest;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:action';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Action';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Action';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/action.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Actions';
+    }
+
+
+}
