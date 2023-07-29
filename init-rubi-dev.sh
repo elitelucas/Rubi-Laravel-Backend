f4b9f8b64152a0220d52ca5619448e32b409a70e
elitelucas <onecodestar@outlook.com>
2023-07-30 00:38:27 +0800

    update dockefile-dev with rubi-init-dev.sh

---
+++ init-rubi-dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+composer dump-autoload
+
+#/usr/local/bin/php /app/artisan config:cache
+#/usr/local/bin/php /app/artisan view:cache
+#/usr/local/bin/php /app/artisan view:clear
+#/usr/local/bin/php /app/artisan route:clear
+
+/usr/local/bin/php /app/artisan optimize:clear
+
+# moment
+# /usr/local/bin/php /app/artisan telescope:install
+
+/usr/local/bin/php /app/artisan migrate:fresh --seed --force
+
+/usr/local/bin/php /app/artisan passport:install --uuids
+
+
+# /usr/local/bin/php /app/artisan migrate:fresh --seed
