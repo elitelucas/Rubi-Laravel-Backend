85b1f9492a558fff5530f5aa0b5af7f9f8d1134c
elitelucas <onecodestar@outlook.com>
2023-07-29 00:12:13 -0300

    test: comment route for collab login

--- init-rubi.sh
+++ init-rubi.sh
@@ -4,15 +4,15 @@ set -e
 
 composer dump-autoload
 
-/usr/local/bin/php /app/artisan optimize
+#/usr/local/bin/php /app/artisan config:cache
+#/usr/local/bin/php /app/artisan view:cache
+#/usr/local/bin/php /app/artisan view:clear
+#/usr/local/bin/php /app/artisan route:clear
 
-/usr/local/bin/php /app/artisan config:cache
-/usr/local/bin/php /app/artisan view:cache
-/usr/local/bin/php /app/artisan view:clear
-/usr/local/bin/php /app/artisan route:clear
+/usr/local/bin/php /app/artisan optimize:clear
 
 # moment
-/usr/local/bin/php /app/artisan telescope:install
+# /usr/local/bin/php /app/artisan telescope:install
 
 /usr/local/bin/php /app/artisan migrate:fresh --seed --force
 
